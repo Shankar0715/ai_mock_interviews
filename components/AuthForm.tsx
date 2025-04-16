53294dcaf66a725013c8c264bca0093d1d0c01d0
Shankar0715 <135619347+Shankar0715@users.noreply.github.com>
2025-04-16 21:37:33 +0530

    colour changes

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -101,11 +101,11 @@ const AuthForm = ({ type }: { type: FormType }) => {
     <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
-          <Image src="/logo.svg" alt="logo" height={32} width={38} />
-          <h2 className="text-primary-100">PrepWise</h2>
+          <Image src="/ai-avatar.png" alt="logo" height={32} width={38} />
+          <h2 className="text-black">PrepWise</h2>
         </div>
 
-        <h3>Practice job interviews with AI</h3>
+        <h3 className="text-black">Practice job interviews with AI</h3>
 
         <Form {...form}>
           <form
@@ -138,13 +138,13 @@ const AuthForm = ({ type }: { type: FormType }) => {
               type="password"
             />
 
-            <Button className="btn" type="submit">
+            <Button className="btn " type="submit">
               {isSignIn ? "Sign In" : "Create an Account"}
             </Button>
           </form>
         </Form>
 
-        <p className="text-center">
+        <p className="text-center text-black">
           {isSignIn ? "No account yet?" : "Have an account already?"}
           <Link
             href={!isSignIn ? "/sign-in" : "/sign-up"}
